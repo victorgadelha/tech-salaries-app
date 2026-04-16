3e6f0d11c8f3c9c17e9a65fe8ef12029728e3405
Victor Gadelha <joaovictor_gadelha@live.com>
2026-04-16 15:18:11 -0300

    criando função de transformação de dados

---
+++ src/02_transform_silver.py
@@ -0,0 +1,36 @@
+import duckdb
+
+def transform_silver():
+    con = duckdb.connect()
+
+    con.execute("""
+        COPY (
+            SELECT 
+                * REPLACE (
+                    LOWER(job_title) AS job_title,
+                    TRY_CAST(work_year AS INTEGER) AS work_year
+                )
+            FROM 'data/bronze/salaries_raw.parquet'
+            WHERE salary > 0 
+              AND salary IS NOT NULL 
+              AND TRY_CAST(work_year AS INTEGER) IS NOT NULL
+        ) 
+        TO 'data/silver/salaries_cleaned.parquet' 
+        (FORMAT 'PARQUET')
+    """)
+
+    con.execute("""
+        COPY (
+            SELECT * FROM 'data/bronze/salaries_raw.parquet'
+            WHERE salary <= 0 
+               OR salary IS NULL 
+               OR TRY_CAST(work_year AS INTEGER) IS NULL
+        ) 
+        TO 'data/quarantine/salaries_anomalies.parquet' 
+        (FORMAT 'PARQUET')
+    """)
+
+    con.close()
+
+if __name__ == "__main__":
+    transform_silver()
